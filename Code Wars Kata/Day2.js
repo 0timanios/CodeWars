c1d47fc3574a99ed2a3de168f9277d66bed03711
0timanios <bltkothmane@gmail.com>
2025-03-24 20:37:07 -0400

    day 2 and vscodeSnippet

---
+++ Code Wars Kata/Day2.js
@@ -0,0 +1,13 @@
+// Kata Link: https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+function numberToString(num) {
+    return num.toString()
+    
+  }
+
+// Kata Link: https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/javascript
+function checkForFactor (base, factor) {
+    const tOf = true;
+    return (base % factor) == 0 ? tOf : !tOf;
+  }
+
+// Kata Link: 
